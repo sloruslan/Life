1031e407d9f61330633ef354516d73c6eebe1a3f
Konstantin Vorobyev <konstantin.31.12@yandex.ru>
2022-06-21 23:45:54 +0300

    Вот теперь точно "Ну пока так"

--- Life.Server.Core_/Contracts/Managers/IFirstGeneration.cs
+++
@@ -1,9 +0,0 @@
-﻿using Life.Server.Core.Contracts.Domain;
-
-namespace Life.Server.Core.Contracts.Managers
-{
-    public interface IFirstGeneration
-    {
-        public static abstract T[,] StartGeneration<T>(int perHorizontal, int perVertical, int density) where T : ICellIsLife, new();
-    }
-}
