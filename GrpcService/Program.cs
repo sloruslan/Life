008e531e1f20b7812ab05cff8d22bdfbc0576ace
Konstantin Vorobyev <konstantin.31.12@yandex.ru>
2022-07-06 00:09:08 +0300

    gRPC big size message

--- GrpcService/Program.cs
+++ GrpcService/Program.cs
@@ -7,7 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
 
 // Add services to the container.
-builder.Services.AddGrpc();
+builder.Services.AddGrpc( options =>
+    {
+        options.MaxReceiveMessageSize = 8192 * 8192;
+        options.MaxSendMessageSize = 8192 * 8192;
+    }
+);
 
 
 var app = builder.Build();
